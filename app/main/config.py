da497481f49731690ce988c414f0ed43bca802f3
Paweł <pawel.mstowski@gmail.com>
2019-01-26 18:58:15 +0100

    Fixes

--- app/main/config.py
+++ app/main/config.py
@@ -10,15 +10,15 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQL_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(basedir, 'flask_boilerplate_main.db'))
-    SQL_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(basedir, 'flask_boilerplate_main.db'))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    SQL_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(basedir, 'flask_boilerplate_test.db'))
-    SQL_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(basedir, 'flask_boilerplate_test.db'))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     PRESERVE_CONTEXT_ON_EXCEPTION = False
 
 
